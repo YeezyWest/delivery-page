58f934d012d7154ebf5636503915d7b22a500c0f
Yusuf olatunji  adeshina <72082053+YeezyWest@users.noreply.github.com>
2022-03-09 16:34:16 +0100

    i added clicks on my codes making an active background and styling the background.

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ const menu = document.querySelector('.mobile-menu');
 btn.addEventListener("click", () => {
     menu.classList.toggle("hidden");
 });
+
+
+// Get the container element
+let transportContainer = document.getElementById("transport");
+
+// Get all buttons with class="btn" inside the container
+let transportType = transportContainer.getElementsByClassName("transport-type");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (let i = 0; i < transportType.length; i++) {
+    transportType[i].addEventListener("click", function () {
+        let current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
